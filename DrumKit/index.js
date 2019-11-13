d037ff79ca27f6809b970725b01d5dfc1a769f45
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-13 15:25:11 +0700

    12. Advanced Javascript and DOM Manipulation: 13. Adding Animation to Websites

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -11,6 +11,8 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
 
         makeSound(buttonInnerHTML);
 
+        buttonAnimation(buttonInnerHTML);
+
     });
 
 }
@@ -21,6 +23,8 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
 document.addEventListener('keypress', function (event) {
 
     makeSound(event.key);
+
+    buttonAnimation(event.key);
 
 });
 
@@ -65,6 +69,18 @@ function makeSound(key) {
         default:
             console.log(key);
     }
+}
+
+
+function buttonAnimation(currentKey) {
+
+    let activeButton = document.querySelector('.' + currentKey);
+
+    activeButton.classList.add('pressed');
+
+    setTimeout(function () {
+        activeButton.classList.remove('pressed');
+    }, 100);
 }
 
 
