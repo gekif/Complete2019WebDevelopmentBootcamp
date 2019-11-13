6aaf2fa1b74bf07e21aa47028c0a2e2f905797ea
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-13 17:41:38 +0700

    18. Express.js with Node.js: 8. Calculator Setup Challenge Solution

---
+++ calculator/calculator.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = new express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+
+app.listen(3000, () => {
+    console.log('Server is listen on port 3000')
+});
