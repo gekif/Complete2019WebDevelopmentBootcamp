4f0607288b21965309296651bdab34edd269b193
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-13 17:35:11 +0700

    18. Express.js with Node.js: 5. Understanding and Working with Routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
 });
 
+app.get('/contact', (req, res) => {
+    res.send("Contact me at: gekif");
+});
+
+app.get('/about', (req, res) => {
+    res.send("Scrum mastah");
+});
+
+app.get('/hobbies', (req, res) => {
+    res.send('<ul><li>Basketball</li><li>Magic</li></ul>');
+});
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
