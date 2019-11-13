781c4aa51022cc5589abeafe8c04cde60990dc9c
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-13 17:47:01 +0700

    18. Express.js with Node.js: 9. Responding to Requests with HTML Files

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = new express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000, () => {
-    console.log('Server is listen on port 3000')
+    console.log('Server is listen on port 3000');
 });
