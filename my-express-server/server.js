020ba5bcaa790379aa06a0817a33699fbddd30b4
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-13 17:17:36 +0700

    18. Express.js with Node.js: 3. Handling Requests and Responses the GET Request

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const app = new express();
 
-app.listen(3000, function () {
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
+
+app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
