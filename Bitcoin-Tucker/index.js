d3fe32752afa21306e5a1e579a30649f00ed1c73
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-14 11:08:27 +0700

    20. APIs - Application Programming Interfaces: 2. Using the Request Module to Get Data from an API

---
+++ Bitcoin-Tucker/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = new express();
+
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
