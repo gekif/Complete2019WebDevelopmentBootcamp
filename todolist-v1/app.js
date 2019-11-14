1463153d48edd21cbbe270394b0ffd7dcad1ac04
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-14 17:50:15 +0700

    21. EJS: 4. Creating Your First EJS Templates

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,20 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const app = express();
 
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
     let today = new Date();
     let currentDay = today.getDay();
+    let day = '';
 
-    if (currentDay === 6 || currentDay === 0) {
-        res.write('<h1>yay it is weekend!</h1>');
-    } else {
-        res.sendFile(__dirname + '/index.html');
+    switch (currentDay) {
+        case 0:
+            day = 'Sunday';
+            break;
+        case 1:
+            day = 'Monday';
+            break;
+        case 2:
+            day = 'Tuesday';
+            break;
+        case 3:
+            day = 'Wednesday';
+            break;
+        case 4:
+            day = 'Thursday';
+            break;
+        case 5:
+            day = 'Friday';
+            break;
+        case 6:
+            day = 'Saturday';
+            break;
+        default:
+            console.log('Error: The current day is equal to: ' + currentDay);
     }
+
+    res.render('list', {kindOfDay: day});
 
 });
 
