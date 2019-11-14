46b64a81702ea180140d8ed891bba4b2c6f9d452
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-14 11:33:21 +0700

    20. APIs - Application Programming Interfaces: 3. Understanding the JSON Format and Working with JSON

--- Bitcoin-Tucker/index.js
+++ Bitcoin-Tucker/index.js
@@ -1,15 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = new express();
 
 const port = 3000;
 
+
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
+
+app.post('/', (req, res) => {
+
+    let crypto = req.body.crypto;
+    let fiat = req.body.fiat;
+
+    let baseURL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
+
+    let finalURL = baseURL + crypto + fiat;
+
+    request(finalURL, (error, response, body) => {
+
+        // console.log(body);
+
+        let data = JSON.parse(body);
+        let price = data.last;
+
+        let currentDate = data.display_timestamp;
+
+        res.write(`<p>The current date is ${currentDate}</p>`);
+
+        res.write(`<h1>The current price of ${crypto} is ${price} ${fiat}</h1>`);
+
+        res.send()
+
+    });
+
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
