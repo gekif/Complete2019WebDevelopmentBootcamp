a4904fc12e46767bcd05a1fd4bf748bc4b3c0a98
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-15 09:49:21 +0700

    21. EJS: 6. Passing Data from Your Webpage to Your Server

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,41 +3,37 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+
+let items = ['Buy cook', 'Cook Food', 'Eat Food'];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get('/', (req, res) => {
 
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = '';
 
-    switch (currentDay) {
-        case 0:
-            day = 'Sunday';
-            break;
-        case 1:
-            day = 'Monday';
-            break;
-        case 2:
-            day = 'Tuesday';
-            break;
-        case 3:
-            day = 'Wednesday';
-            break;
-        case 4:
-            day = 'Thursday';
-            break;
-        case 5:
-            day = 'Friday';
-            break;
-        case 6:
-            day = 'Saturday';
-            break;
-        default:
-            console.log('Error: The current day is equal to: ' + currentDay);
-    }
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
 
-    res.render('list', {kindOfDay: day});
+    let day = today.toLocaleDateString('id-ID', options);
+
+    res.render('list', {kindOfDay: day, newListItems: items});
+
+});
+
+
+app.post('/', (req, res) => {
+    let item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect('/');
 
 });
 
