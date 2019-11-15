7d323e83aa55a0fad9be40f5df80cb13da0728d2
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-15 17:13:36 +0700

    26. Mongoose: 3. Data Validation with Mongoose

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -8,8 +8,15 @@ mongoose.connect('mongodb://localhost:27017/fruitsDB', {
 
 
 const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: [true, 'Please check your data entry, no name specified!~']
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
     review: String
 });
 
@@ -18,13 +25,12 @@ const Fruit = mongoose.model('Fruit', fruitSchema);
 
 
 const fruit = new Fruit({
-    name: "Apple",
-    rating: 7,
-    review: "Pretty solid as a fruit"
+    rating: 20,
+    review: "Peaches are yummy"
 });
 
 
-// fruit.save();
+fruit.save();
 
 
 const personSchema = new mongoose.Schema({
@@ -45,42 +51,10 @@ const person = new Person({
 // person.save();
 
 
-const kiwi = new Fruit({
-    name: "Kiwi",
-    rating: 10,
-    review: "The best fruit"
-});
-
-
-const orange = new Fruit({
-    name: "Orange",
-    rating: 4,
-    review: "Too sour for me"
-});
-
-
-const banana = new Fruit({
-    name: "Banana",
-    rating: 3,
-    review: "Weird texture"
-});
-
-
-/*Fruit.insertMany([kiwi, orange, banana], (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Successfully save all the fruits to fruitsDB');
-    }
-});*/
-
-
 Fruit.find((err, fruits) => {
     if (err) {
         console.log(err);
     } else {
-        // console.log(fruits);
-
         mongoose.connection.close();
 
         fruits.forEach((fruit) => {
@@ -88,6 +62,9 @@ Fruit.find((err, fruits) => {
         });
     }
 });
+
+
+
 
 
 const findDocuments = (db, callback) => {
