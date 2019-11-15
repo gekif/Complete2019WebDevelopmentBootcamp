bab5c880de7e3caed84c0cf7a8f8ab71cfad930a
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-15 21:40:28 +0700

    27. Putting Everything Together: 5. Creating Custom Lists using Express Route Parameters

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -45,6 +45,15 @@ const item3 = new Item({
 const defaultItems = [item1, item2, item3];
 
 
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+};
+
+
+const List = mongoose.model('List', listSchema);
+
+
 app.get("/", function(req, res) {
 
     Item.find({}, function (err, foundItems) {
@@ -96,6 +105,37 @@ app.post('/delete', function (req, res) {
             res.redirect('/');
         }
     });
+});
+
+
+app.get('/:customListName', function (req, res) {
+    const customListName = req.params.customListName;
+
+    List.findOne({name: customListName}, function (err, foundList) {
+
+        if (!err) {
+
+            if (!foundList) {
+
+                // Create a new list
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+
+                list.save();
+
+                res.redirect('/' + customListName);
+
+            } else {
+
+                // Show an existing list
+                res.render('list', {listTitle: foundList.name, newListItems: foundList.items});
+
+            }
+        }
+    });
+
 });
 
 
