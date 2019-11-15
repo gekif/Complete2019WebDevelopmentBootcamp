c4c08cdab4902be1c3e3f6e7a561c2d71c5cf6bd
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-15 16:43:08 +0700

    26. Mongoose: 25. MongoDB: 1. Introduction to Mongoose

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,52 +1,65 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'fruitsDB';
-
-// Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true});
-
-// Use connect method to connect to the Server
-client.connect(function (err) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-
-    const db = client.db(dbName);
-
-    findDocuments(db, () => {
-        client.close();
-    })
+mongoose.connect('mongodb://localhost:27017/fruitsDB', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 });
 
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
+});
 
-const insertDocuments = (db, callback) => {
-    // Get the documents collection
-    const collection = db.collection('fruits');
-    // Insert some documents
-    collection.insertMany([
-        {
-            name: "Apple",
-            score: 8,
-            review: "Great fruit"
-        },
-        {
-            name: "Orange",
-            score: 6,
-            review: "Kinda sour"
-        }
-    ], (err, result) => {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    });
-};
+const Fruit = mongoose.model('Fruit', fruitSchema);
 
+const fruit = new Fruit({
+    name: "Apple",
+    rating: 7,
+    review: "Pretty solid as a fruit"
+});
+
+// fruit.save();
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const person = new Person({
+    name: "John",
+    age: 37
+});
+
+// person.save();
+
+const kiwi = new Fruit({
+    name: "Kiwi",
+    rating: 10,
+    review: "The best fruit"
+});
+
+const orange = new Fruit({
+    name: "Orange",
+    rating: 4,
+    review: "Too sour for me"
+});
+
+const banana = new Fruit({
+    name: "Banana",
+    rating: 3,
+    review: "Weird texture"
+});
+
+Fruit.insertMany([kiwi, orange, banana], (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Successfully save all the fruits to fruitsDB');
+    }
+});
 
 const findDocuments = (db, callback) => {
     // Get the documents collection
