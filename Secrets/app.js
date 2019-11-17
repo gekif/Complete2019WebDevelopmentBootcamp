0a5d28cbe449ca5fe1f0bfac3a81d3b7b0d817d4
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-17 20:06:06 +0700

    31. Authentication & Security: 3. Level 1 - Register Users with Username and Password

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,27 +1,85 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
+
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
+
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+
+const User = new mongoose.model('User', userSchema);
+
+
 app.route('/').get((req, res) => {
     res.render('home');
 });
+
 
 app.route('/login').get((req, res) => {
     res.render('login');
 });
 
+
 app.route('/register').get((req, res) => {
     res.render('register');
 });
 
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
+});
+
+
+app.route('/register').post((req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+
+});
+
+
+app.route('/login').post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render('secrets');
+                }
+            }
+        }
+    });
 });
